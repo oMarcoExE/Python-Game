2158506731c83a0629980da50bc66917c04f6c89
Marco <marco23a@hotmail.com>
2023-10-07 13:00:29 -0300

    salvar

--- game.py
+++ game.py
@@ -1,3 +1,27 @@
 import pygame
 
 pygame.init()
+
+##
+##  Tela
+##
+tamanho = (960, 540)
+tela = pygame.display.set_mode(tamanho)
+pygame.display.set_caption("Luta na ponte")
+
+##
+##Importa imagens
+##
+
+##Pesonagem
+knight_idle = pygame.image.load("assets\knight\1 IDLE_000.png").convert_alpha
+
+##Background
+Background1 = pygame.image.load("assets\background\1\bamboo bridge.png").convert_alpha
+
+
+
+status = True
+## Loop principal
+while True:
+    pass
